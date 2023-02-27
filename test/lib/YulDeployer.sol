65efba6116e08c486bd0c0dadd7870a327201886
Taehwa Kim <hi.taehwa@gmail.com>
2023-02-27 17:48:14 +0900

    fix: some yul commands cause compile error (iszero, eq, etc)

--- test/lib/YulDeployer.sol
+++ test/lib/YulDeployer.sol
@@ -9,7 +9,7 @@ contract YulDeployer is Test {
     ///@param fileName - The file name of the Yul contract. For example, the file name for "Example.yul" is "Example"
     ///@return deployedAddress - The address that the contract was deployed to
     function deployContract(string memory fileName) public returns (address) {
-        string memory bashCommand = string.concat('cast abi-encode "f(bytes)" $(solc --yul yul/', string.concat(fileName, ".yul --bin | tail -1)"));
+        string memory bashCommand = string.concat('cast abi-encode "f(bytes)" $(solc --strict-assembly yul/', string.concat(fileName, ".yul --bin | tail -1)"));
 
         string[] memory inputs = new string[](3);
         inputs[0] = "bash";
